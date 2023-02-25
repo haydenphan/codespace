6149c753dfb79e781094f40e54e86d5d76bcc9d5
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-02-25 15:49:35 +0000

    Sat, Feb 25, 2023, 3:49 PM +00:00

--- week1.c
+++ week1.c
@@ -3,7 +3,7 @@
 
 int main() {
     string name = get_string ("Enter your name: ");
-    string age = get_string ("Enter your age: ");
+    int age = get_int ("Enter your age: ");
     int phone = get_int ("Enter your phone number: ");
-    printf ("%s\n%s\n%d\n", name, age, phone);
+    printf ("%s\n%d\n%d\n", name, age, phone);
 }
