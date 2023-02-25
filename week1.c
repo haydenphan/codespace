2bc327506340d291b9226e3ee0bf2751698ed9b3
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-02-25 15:27:46 +0000

    Sat, Feb 25, 2023, 3:27 PM +00:00

---
+++ week1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    string name, age, phone;
+    printf ("Enter your name: ");
+    name = scanf ("%s", &name);
+    printf ("Enter your age: ");
+    name = scanf ("%s", &age);
+    printf ("Enter your phone number: ");
+    name = scanf ("%s", &phone);
+    printf ("%s%s%s", name, age, phone)
+}
