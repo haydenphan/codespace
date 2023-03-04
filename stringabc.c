d029d0798ec41a8634439311a9003291397fa9ea
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-03-04 07:16:54 +0000

    Sat, Mar 4, 2023, 7:16 AM +00:00

--- stringabc.c
+++ stringabc.c
@@ -2,12 +2,14 @@
 
 int main() {
     int i;
-	char s[];
+	char s[26];
     char j;
-    for (i=0; i<=26; i++) {
+    for (i=0; i<26; i++) {
         for (j='a'; j<='z'; j++) {
 			s[i] = j;
 	    }
     }
+    s[25] = '\0';
 	printf ("%s ", s);
+    return 0;
 }
