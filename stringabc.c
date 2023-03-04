0d665c3e8729b9ec74488abb2e5ae952641e5c81
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-03-04 07:12:10 +0000

    Sat, Mar 4, 2023, 7:12 AM +00:00

--- stringabc.c
+++ stringabc.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int n, i;
-	char s[n];
+    int i;
+	char s[];
     char j;
     for (i=0; i<=n; i++) {
-        n = i;
         for (j='a'; j<='z'; j++) {
-			s[n] = j;
+			s[i] = j;
 	    }
     }
 	printf ("%s ", s);
