739639cd12278ea7acb218e918a5f965498215be
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-03-05 10:53:04 +0000

    Sun, Mar 5, 2023, 10:53 AM +00:00

---
+++ triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void valid_triangle(double a, double b, double c) {
+    if (a<=0 || b<=0 || c<=0) {
+
+    } else if (a+b>=c || a+c>=b || b+c>=a) {
+
+    } else {
+
+    }
+}
+
