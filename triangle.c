bca86a47df325bc802e17b952d562539e7dbd996
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-03-07 06:14:25 +0000

    Tue, Mar 7, 2023, 6:14 AM +00:00

--- triangle.c
+++ triangle.c
@@ -4,7 +4,7 @@
 bool valid_triangle(double a, double b, double c) {
     if (a<=0 || b<=0 || c<=0) {
         return false;
-    } else if (a+b>=c || a+c>=b || b+c>=a) {
+    } else if (a+b<=c || a+c<=b || b+c<=a) {
         return false;
     } else {
         return true;
@@ -17,9 +17,9 @@ int main() {
     scanf ("%lf%lf%lf", &a, &b, &c);
     bool n = valid_triangle(a, b, c);
     if (n == true) {
-        printf ("True");
+        printf ("3 so vua nhap la 3 canh cua tam giac\n");
     } else {
-        printf ("False");
+        printf ("Da xay ra loi! Vui long nhap lai\n");
     }
 }
 
---
+++ workshop04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    int n=260, *p=&n;
+    printf ("n=%d\n", n);
+    *pp=0;
+    printf ("n=%d\n", n);
+    getch();
+}
