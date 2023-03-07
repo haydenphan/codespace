f450c1f582b49ae2b43a0770da6e5411283d706c
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-03-07 06:27:44 +0000

    Tue, Mar 7, 2023, 6:27 AM +00:00

--- workshop04.c
+++ workshop04.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int T(int* p, int* q) {
+void T(int* p, int* q) {
     int t = *p;
     *p = *q;
     *q = t;
 }
 int main() {
-    int a=7, b=6, c=5;
-    int L= t(b, a, c);
-    printf("%d\n", L);
+    int a=7, b=6;
+    T(&a, &b);
+    printf("%d %d\n", a, b);
 }
