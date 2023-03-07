c303082d4337f80ef99579f3a8613164d4ccfb23
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-03-07 09:27:14 +0000

    Tue, Mar 7, 2023, 9:27 AM +00:00

--- workshop04.c
+++ workshop04.c
@@ -5,14 +5,16 @@ int main()
 {
     int n;
     float S = 1; // khởi tạo giá trị S ban đầu là 1
-    float giaithua = 1; // khởi tạo giá trị giaithua ban đầu là 1
 
     printf("Nhap vao so nguyen n: ");
     scanf("%d", &n);
 
     for (int i = 2; i <= n; i++) {
-        giaithua *= i; // tính giai thừa của i
-        S += giaithua / pow(2 * i, 5); // tính phần tử i của biểu thức và cộng vào tổng
+        int giaithua = 1; // khởi tạo giai thừa của i
+        for (int j = 1; j <= i; j++) {
+            giaithua *= j; // tính giai thừa của i
+        }
+        S += (float)giaithua / pow(2 * i, 5); // tính phần tử i của biểu thức và cộng vào tổng
     }
 
     printf("S = %.6f", sqrt(S)); // tính căn bậc hai của tổng và in ra kết quả với 6 chữ số sau dấu phẩy
