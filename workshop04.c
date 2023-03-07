bc9e5d00d9a4668ecf5b91659324f611e0da7a34
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-03-07 08:59:52 +0000

    Tue, Mar 7, 2023, 8:59 AM +00:00

--- workshop04.c
+++ workshop04.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
-int T(int* p, int* q) {
-    int t = (*p) + (*q) > 12 ? 5:6;
-    return 2*t%5;
-}
-int main() {
-    int a=3, b=4, c;
-    c = T(&a, &b);
-    printf("%d\n", c);
+int main()
+{
+    int n;
+    float S = 1; // khởi tạo giá trị S ban đầu là 1
+    float giaithua = 1; // khởi tạo giá trị giaithua ban đầu là 1
+
+    printf("Nhap vao so nguyen n: ");
+    scanf("%d", &n);
+
+    for (int i = 2; i <= n; i++) {
+        giaithua *= i; // tính giai thừa của i
+        S += giaithua / pow(2 * i, 5); // tính phần tử i của biểu thức và cộng vào tổng
+    }
+
+    printf("S = %.6f", sqrt(S)); // tính căn bậc hai của tổng và in ra kết quả với 6 chữ số sau dấu phẩy
+
+    return 0;
 }
