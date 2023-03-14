586a30796d55dc9ed56cb93d11c35fca55da11ce
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-03-14 08:15:25 +0000

    Tue, Mar 14, 2023, 8:15 AM +00:00

--- baislide.c
+++ baislide.c
@@ -1,7 +1,31 @@
+#include <stdio.h>
+int linearsearch (int a[10],int b);
+
 int main() {
-    int i=0, j=0;
-    for (i=1; i<10, j<3; i*=2) {
-        printf("%d ", j++);
+    int n=10;
+    int a[n];
+    int i;
+    for (i=0; i<n; i++) {
+        scanf("%d", &a[i]);
     }
-    printf(" %d %d", i, j);
+    printf ("Nhap so can tim trong mang: ");
+    int b;
+    scanf ("%d", &b);
+    int x = linearsearch (a[n], b);
+    if (x != -1) {
+        printf ("Co");
+    } else {
+        printf ("Khong");
+    }
+    return 0;
+}
+
+int linearsearch (int a[n], int b) {
+    for (i=0; i<n; i++) {
+        if (a[i]==b) {
+            return i;
+        } else {
+            return -1;
+        }
+    }
 }
