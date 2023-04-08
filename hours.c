0543845987561b50eb925301a4079a57b59ce733
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-08 18:13:45 +0000

    Sat, Apr 8, 2023, 6:13 PM +00:00

--- hours.c
+++ hours.c
@@ -1,1 +1,22 @@
-#include <stdio
+#include <stdio.h>
+
+int avr (int* a, int n) {
+    int i, sum=0;
+    for (i=0; i<n; i++) {
+        sum += a[i];
+    }
+    return (double)(sum/n);
+}
+
+int total (int* b, int m) {
+    int i, sum=0;
+    for (i=0; i<n; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
+
+int main() {
+    int 
+    printf ("Number of weeks taking CS50: ");
+}
