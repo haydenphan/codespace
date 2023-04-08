68bacf5bb0256763eb58c5d2ccbad4da03020a89
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-08 18:28:34 +0000

    Sat, Apr 8, 2023, 6:28 PM +00:00

--- hours.c
+++ hours.c
@@ -10,21 +10,31 @@ int avr (int* a, int n) {
 
 int total (int* b, int m) {
     int i, sum=0;
-    for (i=0; i<n; i++) {
-        sum += a[i];
+    for (i=0; i<m; i++) {
+        sum += b[i];
     }
     return sum;
 }
 
 int main() {
-    int n;
+    int i, n;
+    char option;
     printf ("Number of weeks taking CS50: ");
     scanf ("%d", &n);
     int a[n];
-           int i;
-           for (i=0; i<n; i++) {
-             printf ("Week %d HW Hours: ", i);
-             scanf ("%d", a[i]);
-           }
+    for (i=0; i<n; i++) {
+       printf ("Week %d HW Hours: ", i);
+       scanf ("%d", &a[i]);
     }
+    printf ("Enter T for total hours, A for average hours per week: ");
+    scanf ("%c", &option);
+    switch (option) {
+        case 'T':
+           int t = total (&a, n);
+           printf ("%d hours", t);
+        case 'A':
+           double m = avr (&a, n);
+           printf ("%.1lf hours", m);
+    }
+    return 0;
 }
