d27a93502a1941798b9e59262389cd802d08546b
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-09 14:34:27 +0000

    Sun, Apr 9, 2023, 2:34 PM +00:00

--- hours.c
+++ hours.c
@@ -1,58 +1,51 @@
-#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-// The point values of each letter of the alphabet
-const int POINTS[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
+const int point[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
-// Function prototypes
-int compute_score(string word);
-
-int main(void)
-{
-    // Get input words from both players
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
-
-    // Score both words
-    int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
-
-    // Print the winner
-    if (score1 > score2)
-    {
-        printf("Player 1 wins!\n");
-    }
-    else if (score2 > score1)
-    {
-        printf("Player 2 wins!\n");
-    }
-    else
-    {
-        printf("Tie!\n");
-    }
+int total (char* a, int n) {
+	int i, sum=0;
+	for (i=0; i<n; i++) {
+		int letter = toupper(a[i]);
+		if (!isalpha(letter)) {
+			continue;
+		}
+		sum += point [letter - 'A'];
+	}
+	return sum;
 }
 
-int compute_score(string word)
-{
-    int score = 0;
+int compare (int a, int b) {
+	if (a>b) {
+		return 1;
+	} else if (a<b) {
+		return 0;
+	} else {
+		return -1;
+	}
+}
 
-    // Calculate score for each letter in the word
-    for (int i = 0, n = strlen(word); i < n; i++)
-    {
-        // Convert letter to uppercase for indexing into POINTS
-        int letter = toupper(word[i]);
+void print (int x) {
+	if (x==1) {
+		printf ("Player 1 wins!");
+	} else if (x==0) {
+		printf ("Player 2 wins!");
+	} else {
+		printf ("Tie!");
+	}
+}
 
-        // Ignore non-alphabetic characters
-        if (!isalpha(letter))
-        {
-            continue;
-        }
-
-        // Calculate score for letter and add to total score
-        score += POINTS[letter - 'A'];
-    }
-
-    return score;
+int main() {
+	char player1 [100];
+	char player2 [100];
+	printf ("Player 1: ");
+	scanf ("%s", player1);
+	int n = strlen (player1);
+	printf ("Player 2: ");
+	scanf ("%s", player2);
+	int m = strlen (player2);
+	int kq = compare (total(&player1, n), total(&player2, m));
+	print (kq);
+	return 0;
 }
