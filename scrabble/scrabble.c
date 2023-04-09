aea7d711b8f2f7bd7888c355305324ac40e4c933
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-09 14:43:44 +0000

    Sun, Apr 9, 2023, 2:43 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,27 +1,51 @@
-#include <ctype.h>
-#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-// Points assigned to each letter of the alphabet
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+const int point[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
-int compute_score(string word);
-
-int main(void)
-{
-    // Get input words from both players
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
-
-    // Score both words
-    int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
-
-    // TODO: Print the winner
+int total (char* a, int n) {
+	int i, sum=0;
+	for (i=0; i<n; i++) {
+		int letter = toupper(a[i]);
+		if (!isalpha(letter)) {
+			continue;
+		}
+		sum += point [letter - 'A'];
+	}
+	return sum;
 }
 
-int compute_score(string word)
-{
-    // TODO: Compute and return score for string
+int compare (int a, int b) {
+	if (a>b) {
+		return 1;
+	} else if (a<b) {
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+void print (int x) {
+	if (x==1) {
+		printf ("Player 1 wins!");
+	} else if (x==0) {
+		printf ("Player 2 wins!");
+	} else {
+		printf ("Tie!");
+	}
+}
+
+int main() {
+	char player1 [100];
+	char player2 [100];
+	printf ("Player 1: ");
+	scanf ("%s", player1);
+	int n = strlen (player1);
+	printf ("Player 2: ");
+	scanf ("%s", player2);
+	int m = strlen (player2);
+	int kq = compare (total(&player1, n), total(&player2, m));
+	print (kq);
+	return 0;
 }
