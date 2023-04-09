323e5de6543ab7f080fd631cbadbcbbd9e0670fb
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-09 10:05:47 +0000

    Sun, Apr 9, 2023, 10:05 AM +00:00

--- hours.c
+++ hours.c
@@ -1,41 +1,58 @@
+#include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int avr (int* a, int n) {
-    int i, sum=0;
-    for (i=0; i<n; i++) {
-        sum += a[i];
+// The point values of each letter of the alphabet
+const int POINTS[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
+
+// Function prototypes
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
     }
-    return (double)(sum/n);
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
-int total (int* b, int m) {
-    int i, sum=0;
-    for (i=0; i<m; i++) {
-        sum += b[i];
-    }
-    return sum;
-}
+int compute_score(string word)
+{
+    int score = 0;
 
-int main() {
-    int i, n, t;
-    double m;
-    char option;
-    printf ("Number of weeks taking CS50: ");
-    scanf ("%d", &n);
-    int a[n];
-    for (i=0; i<n; i++) {
-       printf ("Week %d HW Hours: ", i);
-       scanf ("%d", &a[i]);
+    // Calculate score for each letter in the word
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        // Convert letter to uppercase for indexing into POINTS
+        int letter = toupper(word[i]);
+
+        // Ignore non-alphabetic characters
+        if (!isalpha(letter))
+        {
+            continue;
+        }
+
+        // Calculate score for letter and add to total score
+        score += POINTS[letter - 'A'];
     }
-    printf ("Enter T for total hours, A for average hours per week: ");
-    scanf ("%c", &option);
-    switch (option) {
-        case 'T':
-           t = total (&a, n);
-           printf ("%d hours", t);
-        case 'A':
-           m = avr (&a, n);
-           printf ("%.1lf hours", m);
-    }
-    return 0;
+
+    return score;
 }
