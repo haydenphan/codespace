85a9827e6a85b2692c135ad5e3ce48e227a322fa
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-11 15:39:31 +0000

    Tue, Apr 11, 2023, 3:39 PM +00:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,18 @@
 int main(void)
 {
     // TODO: Prompt for start size
+    int n,m;
+    do {
+        n = get_int ("Start size: ");
+    } while (n<1)
 
     // TODO: Prompt for end size
-
+    do {
+        m = get_int ("End size: ");
+    } while (m<1)
     // TODO: Calculate number of years until we reach threshold
-
+    for (int i=0; i<;i++) {
+        
+    }
     // TODO: Print number of years
 }
