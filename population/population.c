1c562a02d9122ca63cc4f92e0df3ec61bc95ba96
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-11 15:44:32 +0000

    Tue, Apr 11, 2023, 3:44 PM +00:00

--- population/population.c
+++ population/population.c
@@ -7,17 +7,17 @@ int main(void)
     int n,m;
     do {
         n = get_int ("Start size: ");
-    } while (n<1)
+    } while (n<1);
 
     // TODO: Prompt for end size
     do {
         m = get_int ("End size: ");
-    } while (m<1)
+    } while (m<1);
     // TODO: Calculate number of years until we reach threshold
     int year = -1;
     for (int i=n; i<=m; i*= 13/12) {
         year++;
     }
     // TODO: Print number of years
-    
+    printf ("Years: %d", year);
 }
