d6aa9e15b43275aebf6c187ee6bd93795be3d46d
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-12 16:58:27 +0000

    Wed, Apr 12, 2023, 4:58 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -6,7 +6,7 @@ int main(void)
     int height;
     do {
         height = get_int ("Height: ");
-    } while (height<1);
+    } while (height<1 || height>8);
 
     for (int i = 1; i <= height; i++)
     {
