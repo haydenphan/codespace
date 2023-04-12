881e6dbeb75efbafd37278466d5bd8259d2b9eeb
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-12 16:52:28 +0000

    Wed, Apr 12, 2023, 4:52 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -7,28 +7,22 @@ int main(void)
     do {
         height = get_int ("Height: ");
     } while (height<1);
-   /* for (int i=1; i<=height; i++)
-    {
-        for (int j=height; j>=1; j--)
-        {
-           char k = '#';
-           char p = '#';
-           printf ("%c",k++);
-           */
-      for (int n=height-1; n>=0; n--)
-      {
-        char b = ' ';
-        printf ("%c", b);
-        for (int m=1; m<=height; m++)
-        {
-          char p = '#';
-          printf ("%c", p++);
+    for (int i = 1; i <= height; i++) {
+        // Print spaces before the blocks
+        for (int j = 0; j < height - i; j++) {
+            printf(" ");
         }
-        printf ("\n");
-      }
-
-
-        printf ("\n");
-    
+        // Print left side of the pyramid
+        for (int j = 0; j < i; j++) {
+            printf("#");
+        }
+        // Print gap between the two sides
+        printf("  ");
+        // Print right side of the pyramid
+        for (int j = 0; j < i; j++) {
+            printf("#");
+        }
+        printf("\n");
+    }
     return 0;
 }
