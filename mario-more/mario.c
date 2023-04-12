4e84f2b8d670ff531906027eaa2f1f7c45ef61fb
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-12 16:42:32 +0000

    Wed, Apr 12, 2023, 4:42 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -7,21 +7,15 @@ int main(void)
     do {
         height = get_int ("Height: ");
     } while (height<1);
-    for (int i=1; i<=height; i++)
+   /* for (int i=1; i<=height; i++)
     {
-       /* for (int j=height; j>=1; j--)
+        for (int j=height; j>=1; j--)
         {
            char k = '#';
            char p = '#';
            printf ("%c",k++);
            */
-
-          for (int j=height-1; j>=0; j--) {
-            char a = ' ';
-            printf ("%c", a--);
-          }
-            char b = '#';
-            printf ("%c", b++);
+      
 
 
         printf ("\n");
