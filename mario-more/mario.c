4a93a864cf9e3b38679a21624481ffca27f3e825
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-12 09:41:35 +0000

    Wed, Apr 12, 2023, 9:41 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height = get_int ("Height: ");
+    for (int i=1, int j=1; i<=height, j<=height; i++, j++)
+    {
+        printf ("# #")
+    }
 }
