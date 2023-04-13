a8fcdc17789d554f36ec35bed5883659fd1b13cf
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-13 15:46:16 +0000

    Thu, Apr 13, 2023, 3:46 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,9 +2,31 @@
 #include <stdio.h>
 #include <string.h>
 
-int check (int a[])
+int check (string a[])
 {
-    int n = strlen 
+    int n = strlen (a);
+    string b[n/2];
+    if (n % 2 == 0)
+    {
+        for (int i=0, int j=0; i<n, j<n/2; i+=2, j++)
+        {
+            b[j] = a[i] * 2;
+        }
+    }
+    else
+    {
+        for (int i=1, int j=0; i<n, j<n/2; i+=2, j++)
+        {
+            b[j] = a[i] * 2;
+        }
+    }
+    int sum = 0;
+    for (int k=0; k<n/2; k++)
+    {
+        sum += b[k];
+    }
+    int sum2 = sum;
+    
 }
 
 int main(void)
