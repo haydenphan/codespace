7612b2333786b4dcfbb005cd16a1abed46eb428a
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-13 16:08:26 +0000

    Thu, Apr 13, 2023, 4:08 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -30,10 +30,23 @@ int check (char a[])
     }
     else
     {
+        for (int i=1; i<n; i+=2)
+        {
+            for (int j=0; j<n/2; j++)
+            {
+                b[j] = a[i] * 2;
+            }
+            for (int k=0; k<n-n/2; k++)
+            {
+                c[k] = a[i+1];
+            }
+        }
+        /*
         for (int i=1, int j=0; i<n, j<n/2; i+=2, j++)
         {
             b[j] = a[i] * 2;
         }
+        */
     }
     int sum = 0;
     for (int k=0; k<n/2; k++)
@@ -41,10 +54,36 @@ int check (char a[])
         sum += b[k];
     }
     int sum2 = sum;
-
+    for (int i=0; i<n-n/2; i++)
+    {
+        sum2 += c[i];
+    }
+    if (sum2 % 10 == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int main(void)
 {
-
+    char a[20];
+    do
+    {
+        printf ("Number: ");
+        scanf ("%[0-9]", a);
+    } while (a <= '0' || a >= '9');
+    int kq = check (a);
+    if (kq == 1)
+    {
+        printf ("VALID");
+    }
+    else
+    {
+        printf ("VISA");
+    }
+    return 0;
 }
