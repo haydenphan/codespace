4e1cef4ea9ea2d4a0553c66adaf0e2b39bb695bb
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-13 16:42:59 +0000

    Thu, Apr 13, 2023, 4:42 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,64 +11,55 @@ int check (char a[])
     else
     {
         int n = strlen (a);
-    char b[n/2];
-    char c[n-n/2];
-    if (n % 2 == 0)
-    {
-        for (int i=0; i<n; i+=2)
+        char b[n/2];
+        char c[n-n/2];
+        if (n % 2 == 0)
         {
-            for (int j=0; j<n/2; j++)
+            for (int i=0; i<n; i+=2)
             {
-                b[j] = a[i] * 2;
+                for (int j=0; j<n/2; j++)
+                {
+                   b[j] = a[i] * 2;
+                }
+                for (int k=0; k<n-n/2; k++)
+                {
+                   c[k] = a[i+1];
+                }
             }
-            for (int k=0; k<n-n/2; k++)
+         }
+         else
+         {
+            for (int i=1; i<n; i+=2)
             {
-                c[k] = a[i+1];
+                for (int j=0; j<n/2; j++)
+                {
+                    b[j] = a[i] * 2;
+                }
+                for (int k=0; k<n-n/2; k++)
+                {
+                    c[k] = a[i+1];
+                }
             }
-        }
-  
-
+         }
+         int sum = 0;
+         for (int k=0; k<n/2; k++)
+         {
+            sum += b[k];
+         }
+         int sum2 = sum;
+         for (int i=0; i<n-n/2; i++)
+         {
+            sum2 += c[i];
+         }
+         if (sum2 % 10 == 0)
+         {
+            return 1;
+         }
+         else
+         {
+            return 0;
+         }
     }
-    else
-    {
-        for (int i=1; i<n; i+=2)
-        {
-            for (int j=0; j<n/2; j++)
-            {
-                b[j] = a[i] * 2;
-            }
-            for (int k=0; k<n-n/2; k++)
-            {
-                c[k] = a[i+1];
-            }
-        }
-        /*
-        for (int i=1, int j=0; i<n, j<n/2; i+=2, j++)
-        {
-            b[j] = a[i] * 2;
-        }
-        */
-    }
-    int sum = 0;
-    for (int k=0; k<n/2; k++)
-    {
-        sum += b[k];
-    }
-    int sum2 = sum;
-    for (int i=0; i<n-n/2; i++)
-    {
-        sum2 += c[i];
-    }
-    if (sum2 % 10 == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-    }
-
 }
 
 int checkcheck (char a[])
