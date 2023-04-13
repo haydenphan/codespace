561a50b411f656407a9c370fbf159d09d620e74a
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-13 09:28:06 +0000

    Thu, Apr 13, 2023, 9:28 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,8 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_cents(void);
-int calculate_quarters(int cents);
+int get_cents(void)
+{
+    do
+    {
+        int cents = get_int ("Change owed: ");
+    } while (cents < 1);
+    return cents;
+}
+
+int calculate_quarters(int cents)
+{
+    if (cents >= 25) {
+        while (cents >= 25)
+        {
+            cents -= 25;
+        }
+    }
+}
+
 int calculate_dimes(int cents);
 int calculate_nickels(int cents);
 int calculate_pennies(int cents);
