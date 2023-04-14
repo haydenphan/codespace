062f6905354caa5420cbf6bf4261d478983bff4a
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-14 16:57:51 +0000

    Fri, Apr 14, 2023, 4:57 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,8 +8,37 @@ int count_letters (string s)
     int count = 0;
     for (int i=0; i<strlen(s); i++)
     {
-        toupper (s[i]);
         if (s[i]>='A' && s[i]<='Z')
+        {
+            count++;
+        }
+        else if (s[i]>='a' && s[i]<='z')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_words (string s)
+{
+    int count = 0;
+    for (int i=0; i<strlen(s); i++)
+    {
+        if (s[i]==' ' && s[i+1] != ' ')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_sentences (string s)
+{
+    int count = 0;
+    for (int i=0; i<strlen(s); i++)
+    {
+        if (s[i]=='.' || s[i] == '!' || s[i] == '?')
         {
             count++;
         }
@@ -19,5 +48,7 @@ int count_letters (string s)
 
 int main(void)
 {
-
+    string s = get_string ("Text: ");
+    printf ("%d letters", count_letters(s));
+    return 0;
 }
