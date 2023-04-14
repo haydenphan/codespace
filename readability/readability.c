f981552f3bb6631671b97c2a025fac2c0eb7758c
haydenphan <126269227+haydenphan@users.noreply.github.com>
2023-04-14 16:50:46 +0000

    Fri, Apr 14, 2023, 4:50 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(void) 
+int count_letters (string s)
+{
+    int count = 0;
+    for (int i=0; i<strlen(s); i++)
+    {
+        toupper (s[i]);
+        if (s[i]>='A' && s[i]<='Z')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main(void)
 {
 
 }
